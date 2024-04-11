402f370e100e8a1d0aee0294fedc124e096954fd
marta pedriel <martainesped@gmail.com>
2024-04-11 20:14:16 -0300

    Primer commit

---
+++ pages/01_🚀_Acerca de mi.py
@@ -0,0 +1,22 @@
+import streamlit as st
+st.markdown('Soy Marta Pedriel - Licenciada en Sistemas')
+
+
+texto_largo = """Licenciada en Sistemas con sólida experiencia en programación y análisis. Mi destreza radica en la capacidad para comprender y analizar código escrito por otros, facilitando una rápida interpretación y colaboración efectiva.
+
+🏥 Contribuí significativamente a la migración de datos en una destacada empresa del sector de la salud, brindando atención a usuarios de sistemas en áreas de hardware y software. Mi experiencia incluye la elaboración de informes detallados y precisos.
+
+💼 En mi trayectoria, he trabajado en consultoras de sistemas, participando activamente en el desarrollo y asumiendo el rol de Jefa de Mantenimiento de Sistemas. Esta posición me permitió liderar equipos, asegurando el correcto funcionamiento y mantenimiento de sistemas críticos.
+
+🔍 Recientemente recibida de Data Scientist, complementando mi perfil con habilidades analíticas avanzadas y la capacidad de extraer conocimiento valioso a partir de datos.
+
+🚀 Mi actitud positiva, agilidad para el aprendizaje y habilidades críticas, combinadas con fuertes destrezas interpersonales, me destacan como un colaborador eficiente en entornos colaborativos. Estoy entusiasmada por seguir creciendo y aportando al mundo de la tecnología y la ciencia de datos.
+
+Objetivos: Mi meta es fusionar mi experiencia en desarrollo y análisis con mis habilidades emergentes como Data Scientist. Aspiro a contribuir a proyectos innovadores donde pueda aplicar mi conocimiento técnico y liderazgo, impulsando soluciones tecnológicas efectivas y generando impacto en el ámbito de la ciencia de datos.
+
+Contáctame en 😀:
+👩‍💻 GitHub: https://github.com/martaped 
+
+🚀 LinkedIn: https://www.linkedin.com/in/marta-pedriel", height=200)"""
+
+st.write(texto_largo)
