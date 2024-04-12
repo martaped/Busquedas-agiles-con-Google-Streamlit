f9e0bfedb632baa4f5233deaf6423d1043bc8874
marta pedriel <martainesped@gmail.com>
2024-04-11 22:12:37 -0300

    Primer commit

--- Buscar.py
+++ Buscar.py
@@ -7,14 +7,14 @@ st.markdown('### Retorna los link con ese contenido')
 st.sidebar.markdown('Quien soy')
 query = st.text_input("Ingresa texto que deseas buscar", "Escribe aquí...")
 # Número de resultados máximos
-num_results = st.number_input("Cantidad de Respuestas máxima", min_value=1)
+num_results = st.number_input("Cantidad de Respuestas máxima", min_value=10)
 
 # Opciones de búsqueda
 search_options = {
-    "num_results": num_results-1,
-    "advanced": False,
-    
-    
+    "num_results": num_results-1
+    ,
+    "advanced": False
+      
 }
 
 if st.checkbox("Comienza la búsqueda"):
