0061b163dd6e7f79a144c5804b790b2cd4082bb7
marta pedriel <martainesped@gmail.com>
2024-04-14 22:14:14 -0300

    seg commit

--- pages/02_🛫_extrae_mails.py
+++ pages/02_🛫_extrae_mails.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import re
 import os
+import tempfile
 
 # Función para extraer direcciones de correo electrónico de un texto
 def extraer_emails(texto):
@@ -9,6 +10,12 @@ def extraer_emails(texto):
     # Buscar todas las coincidencias en el texto
     emails_encontrados = re.findall(patron_email, texto)
     return emails_encontrados
+
+def guardar_archivo_temporal(archivo):
+    fd, ruta_archivo_temporal = tempfile.mkstemp()
+    with open(ruta_archivo_temporal, 'wb') as f:
+        f.write(archivo.read())
+    return ruta_archivo_temporal
 
 def main():
     st.title("Extractor de Direcciones de Correo Electrónico")
@@ -20,10 +27,10 @@ def main():
         try:
             texto_original = archivo_cargado.read().decode("utf-8")
 
-            # Obtener la ruta del archivo cargado
-            nombre_archivo = archivo_cargado.name
-            ruta_archivo = os.path.dirname(nombre_archivo)
-            st.write("Directorio del archivo cargado:", ruta_archivo)
+            # Guardar el archivo cargado en un archivo temporal
+            ruta_archivo_temporal = guardar_archivo_temporal(archivo_cargado)
+            ruta_directorio = os.path.dirname(ruta_archivo_temporal)
+            st.write("Directorio del archivo cargado:", ruta_directorio)
 
             # Extraer las direcciones de correo electrónico del texto original
             emails = extraer_emails(texto_original)
@@ -37,11 +44,12 @@ def main():
             nombre_archivo_destino = st.text_input("Ingrese el nombre del archivo de destino (sin extensión):")
             if nombre_archivo_destino:
                 nombre_archivo_destino += ".txt"
-                with open(nombre_archivo_destino, 'w', encoding='utf-8') as archivo_destino:
+                ruta_destino = os.path.join(ruta_directorio, nombre_archivo_destino)
+                with open(ruta_destino, 'w', encoding='utf-8') as archivo_destino:
                     for email in emails:
                         archivo_destino.write(email + '\n')
 
-                st.success(f"Las direcciones de correo electrónico se han guardado en {nombre_archivo_destino}")
+                st.success(f"Las direcciones de correo electrónico se han guardado en {ruta_destino}")
         except UnicodeDecodeError:
             st.error("No se puede decodificar el archivo. Asegúrate de que el archivo sea de texto plano y esté codificado en UTF-8.")
 
